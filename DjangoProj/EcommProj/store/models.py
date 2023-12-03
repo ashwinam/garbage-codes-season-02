ab85bb1fe093a71b6d2e332fe02dcdec2c497567
ashwinPy <ashwinam31@gmail.com>
2023-12-03 17:00:21 +0530

    Product, Customer, Order, Address model classes added

--- DjangoProj/EcommProj/store/models.py
+++ DjangoProj/EcommProj/store/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+class Customer(models.Model):
+    MEMBERSHIP_CHOICES = [
+        ('B', 'Bronze'),
+        ('S', 'Silver'),
+        ('G', 'Gold')
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateTimeField(null=True)
+    membership = models.CharField(max_length=10, choices=MEMBERSHIP_CHOICES, default='B')
+
+class Order(models.Model):
+    PAYMENT_STATUS = [
+        ('P', 'Pending'),
+        ('C', 'Completed'),
+        ('F', 'Failed'),
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=10, choices=PAYMENT_STATUS, default='P')
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    # In relationship think as a in child class specify the parent class so every customer has a address so customer is parent class and address is child class
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    
