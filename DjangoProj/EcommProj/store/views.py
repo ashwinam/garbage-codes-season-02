0bb0f078804c099a582486e5ff5e8d18b5ebf743
ashwinPy <ashwinam31@gmail.com>
2023-12-06 19:40:26 +0530

    learning serializing objects

--- DjangoProj/EcommProj/store/views.py
+++ DjangoProj/EcommProj/store/views.py
@@ -1,13 +1,21 @@
+from .models import Product
+from .serializers import ProductSerializer
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 @api_view()
 def product_list(request):
-    return Response('ok')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 @api_view()
 def product_details(request, id):
-    return Response(id)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+    
