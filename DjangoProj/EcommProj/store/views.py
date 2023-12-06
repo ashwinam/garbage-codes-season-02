6e8917780dcda5c7001ac900a6047a7ffdb9f94f
ashwinPy <ashwinam31@gmail.com>
2023-12-06 18:01:44 +0530

    serializer created

--- DjangoProj/EcommProj/store/views.py
+++ DjangoProj/EcommProj/store/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view()
 def product_list(request):
-    return HttpResponse('ok')
+    return Response('ok')
+
+@api_view()
+def product_details(request, id):
+    return Response(id)
