a62dac618fc2a73308b494ad972d9b5a540956d6
ashwinPy <ashwinam31@gmail.com>
2023-12-07 19:15:39 +0530

    Model serializer

--- DjangoProj/EcommProj/store/serializers.py
+++ DjangoProj/EcommProj/store/serializers.py
@@ -3,14 +3,21 @@ from .models import Collection, Product
 from decimal import Decimal
 from rest_framework import serializers
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
+    # id = serializers.IntegerField()
+    # title = serializers.CharField(max_length=255)
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     collection = serializers.HyperlinkedRelatedField(view_name='collection-details', queryset=Collection.objects.all())
 
