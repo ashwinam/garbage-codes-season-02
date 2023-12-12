ddef8e23e588892e7dd783e10d3ef37de2766576
ashwinPy <ashwinam31@gmail.com>
2023-12-12 09:59:21 +0530

    yesterday changes

--- DjangoProj/DjangoTaskProj/taskApi/urls.py
+++ DjangoProj/DjangoTaskProj/taskApi/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('tasks/', views.TaskApiView.as_view()),
+    path('tasks/<int:id>/', views.TaskApiView.as_view())
 ]
--- DjangoProj/DjangoTaskProj/taskApi/views.py
+++ DjangoProj/DjangoTaskProj/taskApi/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import TaskModels
+from .serializers import TaskSerializer
+
+
+class TaskApiView(APIView):
+
+    def get(self, request, id=None):
+        if id is not None:
+            task = get_object_or_404(TaskModels, pk=id)
+            serializer = TaskSerializer(task)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        queryset = TaskModels.objects.all()
+        serializer = TaskSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def put(self, request, id):
+        try:
+            task = get_object_or_404(TaskModels, pk=id)
+            serializer = TaskSerializer(task, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    def delete(self, request, id):
+        task = get_object_or_404(TaskModels, pk=id)
+        task.delete()
+        return Response({'message': 'Task deleted successfully.'}, status=status.HTTP_200_OK)
