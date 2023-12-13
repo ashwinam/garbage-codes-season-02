58dd9aaa286f349c6dcab2d21c48409440bd7f56
ashwinPy <ashwinam31@gmail.com>
2023-12-13 18:57:40 +0530

    models initialized

--- DjangoProj/StudentManagement/student_api/models.py
+++ DjangoProj/StudentManagement/student_api/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class StudentMainModel(models.Model):
+    name = models.CharField(max_length=255)
+    data_of_birth = models.DateField()
+    gender = models.CharField(max_length=1, choices=[("M", "Male"), ("F", "Female"), ("O", "Other")])
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class StudentMarksModel(models.Model):
+    SEMESTER = [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+        (7, 7),
+        (8, 8),
+    ]
+    student = models.ForeignKey(StudentMainModel, on_delete=models.CASCADE, related_name='students')
+    marks = models.IntegerField()
+    semester = models.CharField(max_length=1, choices=SEMESTER)
+
+    def __str__(self) -> str:
+        return self.student.name
+    
+class StudentMarksMainModel(models.Model):
+    BRANCH = [
+        ('Mtech', 'Mtech'),
+        ('CSC', 'CSC'),
+        ('ECE', 'ECE'),
+        ('IT', 'IT'),
+        ('CIVIL', 'CIVIL'),
+    ]
+    student = models.OneToOneField(StudentMainModel, primary_key=True, on_delete=models.CASCADE, related_name='student_marks')
+    marks = models.ManyToManyField(StudentMainModel)
+    branch = models.CharField(max_length=50, choices=BRANCH)
+
+    def __str__(self) -> str:
+        return self.student.name
