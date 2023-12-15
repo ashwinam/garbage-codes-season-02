f9d940f9b5748441006fd0e999638d7f3a096393
ashwinPy <ashwinam31@gmail.com>
2023-12-15 13:39:59 +0530

    blog list completed

--- DjangoProj/Techzy Blogs/blogs/serializers.py
+++ DjangoProj/Techzy Blogs/blogs/serializers.py
@@ -1,4 +1,5 @@
-from pyexpat import model
+from datetime import datetime
+
 from rest_framework import serializers
 
 from .models import BlogModel
@@ -7,3 +8,8 @@ class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogModel
         fields = ('title', 'content', 'publication_date', 'author')
+    
+    publication_date = serializers.SerializerMethodField(method_name='format_date')
+
+    def format_date(self, object):
+        return object.publication_date.strftime("%B %d, %Y")
--- DjangoProj/Techzy Blogs/blogs/views.py
+++ DjangoProj/Techzy Blogs/blogs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import BlogModel
+from .serializers import BlogSerializer
+
+@api_view(['GET', 'POST'])
+def blog_list(request):
+    if request.method == 'GET':
+        queryset = BlogModel.objects.all()
+        serializer = BlogSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = BlogSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
