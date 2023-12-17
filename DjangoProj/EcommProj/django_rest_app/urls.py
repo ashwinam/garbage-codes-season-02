1c59fdcc672915510d301adb665389f3f65f3f2e
ashwinPy <ashwinam31@gmail.com>
2023-12-17 20:21:57 +0530

    changes in base url

--- DjangoProj/EcommProj/django_rest_app/urls.py
+++ DjangoProj/EcommProj/django_rest_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('practice/', include('playground.urls')),
-    path('', include('store.urls'))
+    path('store/', include('store.urls'))
 ]
--- DjangoProj/EcommProj/store/urls.py
+++ DjangoProj/EcommProj/store/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 from rest_framework.routers import SimpleRouter, DefaultRouter
@@ -7,12 +7,12 @@ router = DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
 
-# urlpatterns = [
-
-#     path('products/', views.ProductList.as_view()),
-#     path('products/<int:pk>/', views.ProductDetails.as_view()),
-#     path('collections/', views.CollectionList.as_view()),
-#     path('collections/<int:pk>/', views.CollectionDetails.as_view(), name='collection-details'),
-# ]
+urlpatterns = [
+    path('', include(router.urls))
+    # path('products/', views.ProductList.as_view()),
+    # path('products/<int:pk>/', views.ProductDetails.as_view()),
+    # path('collections/', views.CollectionList.as_view()),
+    # path('collections/<int:pk>/', views.CollectionDetails.as_view(), name='collection-details'),
+]
