13c0c4dfc4b8da686760cd67aeda49415b207c3f
ashwinPy <ashwinam31@gmail.com>
2023-12-18 17:50:47 +0530

    changes in cart and cartItem model

--- DjangoProj/EcommProj/store/models.py
+++ DjangoProj/EcommProj/store/models.py
@@ -1,5 +1,6 @@
-from itertools import product
+from email.policy import default
 from django.db import models
+from uuid import uuid4
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
@@ -50,12 +51,16 @@ class OrderItem(models.Model):
     unit_price = models.DecimalField(max_digits=6, decimal_places=2) # for fluctuating price
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+
+    class Meta:
+        unique_together = [['cart', 'product']]
 
 
 class Address(models.Model):
