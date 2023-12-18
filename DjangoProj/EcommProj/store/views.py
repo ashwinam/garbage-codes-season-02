e3d814ed5faeb9d8872c565d7b535845fd08f686
ashwinPy <ashwinam31@gmail.com>
2023-12-18 14:19:33 +0530

    searching filter added

--- DjangoProj/EcommProj/store/views.py
+++ DjangoProj/EcommProj/store/views.py
@@ -6,18 +6,18 @@ from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
 
-from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import status
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.select_related('collection').all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
 
 
     def get_serializer_context(self):
