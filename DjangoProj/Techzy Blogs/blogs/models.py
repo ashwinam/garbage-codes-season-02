c3e4d11506e57385a1c8ab2f46118c69e07da0c2
ashwinPy <ashwinam31@gmail.com>
2023-12-22 20:08:36 +0530

    change user module

--- DjangoProj/Techzy Blogs/blogs/models.py
+++ DjangoProj/Techzy Blogs/blogs/models.py
@@ -1,18 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User
-
+from django.conf import settings
 class BlogModel(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     publication_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes', blank=True)
 
     def __str__(self) -> str:
         return self.title
     
 class Comments(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     comment = models.TextField()
     blog = models.ForeignKey(BlogModel, on_delete=models.CASCADE)
 
