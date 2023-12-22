6393ce14ee9a6b4d01be5941fc432485360ef5e0
ashwinPy <ashwinam31@gmail.com>
2023-12-22 20:18:54 +0530

    register models in admin

--- DjangoProj/Techzy Blogs/blogs/admin.py
+++ DjangoProj/Techzy Blogs/blogs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.BlogModel)
+class BlogAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['author']
+    list_display = ['title', 'content', 'publication_date', 'author']
+    list_editable = ['author']
+    search_fields = ['title']
