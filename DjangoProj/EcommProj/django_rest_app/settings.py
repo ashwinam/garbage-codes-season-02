e873999e275ae285b13ba64fae87f7899e7a720f
ashwinPy <ashwinam31@gmail.com>
2023-12-23 14:05:10 +0530

    change the user serializer

--- DjangoProj/EcommProj/django_rest_app/settings.py
+++ DjangoProj/EcommProj/django_rest_app/settings.py
@@ -151,5 +151,6 @@ AUTH_USER_MODEL = 'core.User'
 DJOSER = {
     'SERIALIZERS': {
         'user_create': 'core.serializers.UserCreateSerializer',
+        'current_user': 'core.serializers.UserSerializer',
     }
 }
