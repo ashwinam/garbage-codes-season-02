50beef8162e998ac894a9e5bbcc2ff26a3630f3b
ashwinPy <ashwinam31@gmail.com>
2023-12-30 19:00:52 +0530

    Question 9 solved

---
+++ python exercises/W3 resource/python basics (part - 1)/Que-09.py
@@ -0,0 +1,10 @@
+"""
+9. Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+exam_st_date = (11, 12, 2014)
+Sample Output : The examination will start from : 11 / 12 / 2014
+"""
+
+exam_st_date = (11, 12, 2014)
+day, month, year = exam_st_date
+
+print(f"{day}/{month}/{year}")
