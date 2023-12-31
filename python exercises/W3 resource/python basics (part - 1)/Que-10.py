7497e7c0844262b80a57b80458980d2328816f42
ashwinPy <ashwinam31@gmail.com>
2023-12-31 12:27:51 +0530

    Question 10 solved other solution

--- python exercises/W3 resource/python basics (part - 1)/Que-10.py
+++ python exercises/W3 resource/python basics (part - 1)/Que-10.py
@@ -8,3 +8,15 @@ inp = 5
 # inp + inpinp + inpinpinp # 5+55+555
 
 print(eval(f"{inp}+{inp}{inp}+{inp}{inp}{inp}")) 
+
+# Other solution
+# Prompt the user to input an integer and store it in the variable 'a'
+a = int(input("Input an integer: "))
+
+# Create new integers 'n1', 'n2', and 'n3' by concatenating 'a' with itself one, two, and three times, respectively
+n1 = int("%s" % a)          # Convert 'a' to an integer
+n2 = int("%s%s" % (a, a))   # Concatenate 'a' with itself and convert to an integer
+n3 = int("%s%s%s" % (a, a, a))  # Concatenate 'a' with itself twice and convert to an integer
+
+# Calculate the sum of 'n1', 'n2', and 'n3' and print the result
+print(n1 + n2 + n3)
