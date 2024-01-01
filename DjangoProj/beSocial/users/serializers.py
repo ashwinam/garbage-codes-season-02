e5552f5972ca691e96f409cdf5f5ba52eee7ae19
ashwinPy <ashwinam31@gmail.com>
2024-01-01 19:33:52 +0530

    custom serializers for djoser

---
+++ DjangoProj/beSocial/users/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['username', 'email', 'password', 'first_name', 'last_name']
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
