2c141de58df9857fa1ecce21f30f6bbf0e47c16c
ashwinPy <ashwinam31@gmail.com>
2024-01-01 11:55:06 +0530

    add custom user model

--- DjangoProj/beSocial/users/models.py
+++ DjangoProj/beSocial/users/models.py
@@ -1,3 +1,6 @@
+from enum import unique
 from django.db import models
+from django.contrib.auth. models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
