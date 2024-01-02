37bd5b055bc437e3226ee2792e568e0ea706f819
ashwinPy <ashwinam31@gmail.com>
2024-01-02 19:55:40 +0530

    permissions declared for http methods

--- DjangoProj/beSocial/posts/serializer.py
+++ DjangoProj/beSocial/posts/serializer.py
@@ -5,4 +5,11 @@ from .models import Posts
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Posts
-        fields = ['id', 'title', 'content', 'created_at', 'author']
+        fields = ['id', 'title', 'content', 'created_at']
+    
+    def create(self, validated_data):
+        ModelClass = self.Meta.model
+        instance = ModelClass.objects.create(author=self.context.get('current_user'), **validated_data)
+        return instance
+
+    
--- DjangoProj/beSocial/posts/views.py
+++ DjangoProj/beSocial/posts/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Posts
 from .serializer import PostSerializer
@@ -7,3 +8,13 @@ from .serializer import PostSerializer
 class PostsViewSet(ModelViewSet):
     queryset = Posts.objects.all()
     serializer_class = PostSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated()]
+        return []
+        
+
+    def get_serializer_context(self):
+        print(self.request.user)
+        return {'current_user': self.request.user}
