0872887a940a6fb18ff39f3751c4ed7d93df725d
ashwinPy <ashwinam31@gmail.com>
2024-01-02 19:05:19 +0530

    models finished

--- DjangoProj/beSocial/posts/models.py
+++ DjangoProj/beSocial/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Posts(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="posts")
+
+    def __str__(self) -> str:
+        return self.title
