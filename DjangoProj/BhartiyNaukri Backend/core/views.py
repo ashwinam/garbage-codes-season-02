60949ca07534a479f7e4174a9d198ab42197a7f2
ashwinPy <ashwinam31@gmail.com>
2024-01-07 18:48:52 +0530

    8/1/24 work

--- DjangoProj/BhartiyNaukri Backend/core/views.py
+++ DjangoProj/BhartiyNaukri Backend/core/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
+import djoser.serializers # lookout for user create and showing user 
+import djoser.views # views for showing users nd creating users
 # Create your views here.
