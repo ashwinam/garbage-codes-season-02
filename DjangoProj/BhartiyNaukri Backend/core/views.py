64cc36811ca2672a2d74b92a4fb89a1357a5ee5b
ashwinPy <ashwinam31@gmail.com>
2024-01-09 12:06:03 +0530

    changes in core.views

--- DjangoProj/BhartiyNaukri Backend/core/views.py
+++ DjangoProj/BhartiyNaukri Backend/core/views.py
@@ -1,4 +1,1 @@
 from django.shortcuts import render
-import djoser.serializers # lookout for user create and showing user 
-import djoser.views # views for showing users nd creating users
-# Create your views here.
