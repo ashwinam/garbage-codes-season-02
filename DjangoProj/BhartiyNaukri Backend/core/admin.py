cce0381217b91d1cc23007c3bf42e340a5628b1b
ashwinPy <ashwinam31@gmail.com>
2024-01-09 12:05:11 +0530

    Useradmin added

--- DjangoProj/BhartiyNaukri Backend/core/admin.py
+++ DjangoProj/BhartiyNaukri Backend/core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+@admin.register(get_user_model())
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "user_type")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "user_type")
