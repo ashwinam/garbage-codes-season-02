c51e68f730f09d5e9c1fbb51873de2be501e1e34
ashwinPy <ashwinam31@gmail.com>
2024-01-12 12:32:33 +0530

    Question 49 solved with isfile method

--- python exercises/W3 resource/python basics (part - 1)/Que-49.py
+++ python exercises/W3 resource/python basics (part - 1)/Que-49.py
@@ -2,7 +2,16 @@
 49. Write a Python program to list all files in a directory.
 """
 
+from genericpath import isfile
 import os
 
 ls = os.popen("ls")
-print(ls.read())
+# print(ls.read())
+
+# other solution
+
+cwd = os.getcwd()
+
+for file in os.listdir():
+    if os.path.isfile(file):
+        print(file)
