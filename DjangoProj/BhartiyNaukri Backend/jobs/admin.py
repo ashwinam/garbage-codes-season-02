82be1a8b48b77b2b2d430f717e54334d8d70e874
ashwinPy <ashwinam31@gmail.com>
2024-01-17 13:50:45 +0530

    add: admin

--- DjangoProj/BhartiyNaukri Backend/jobs/admin.py
+++ DjangoProj/BhartiyNaukri Backend/jobs/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Jobs, Categories, CompanyImages
 
 # Register your models here.
+
+@admin.register(Jobs)
+class JobsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(CompanyImages)
+class CompanyImagesAdmin(admin.ModelAdmin):
+    pass
