53d2c3ed60e1b03ff958ecf75943790eab14c6ac
ashwinPy <ashwinam31@gmail.com>
2024-01-18 11:53:40 +0530

    solved: que 84

---
+++ python exercises/W3 resource/python basics (part - 1)/Que-84.py
@@ -0,0 +1,17 @@
+"""
+84. Write a Python program to count the number of occurrences of a specific character in a string.
+"""
+
+string = "assassination"
+
+char = "s"
+
+occurances = dict()
+
+for each_char in string:
+    if each_char in occurances:
+        occurances[each_char] += 1
+    else:
+        occurances[each_char] = 1
+
+print(occurances[char])
