eba086939d765f70e98cf60897bb523943d17815
ashwinPy <ashwinam31@gmail.com>
2024-02-19 13:02:39 +0530

    js-basics: variables

--- FrontEnd stuffs/JavaScript/js-basics/index.js
+++ FrontEnd stuffs/JavaScript/js-basics/index.js
@@ -1,2 +1,14 @@
 // JS file
 console.log("Hello WOrld!");
+
+// js-basics : Variables & constants
+
+let firstName = "Ashwin" ;
+console.log(firstName);
+
+// variable rule
+// 1) variable name shouldn't be reserved keywords
+// 2) should be meaningful
+// 3) cannot start with numbers
+// 4) cannot include space and hyphen
+
