57a4e8934a5776fcbf4fd819d4961c8f7aa2f1b7
ashwinPy <ashwinam31@gmail.com>
2024-02-21 14:44:44 +0530

    js-exercise: 03 clean code

--- FrontEnd stuffs/JavaScript/beginner exercises/exercise-03.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/exercise-03.js
@@ -13,12 +13,17 @@ const output = fizzBuzz('15');
 console.log(output);
 
 function fizzBuzz(input) {
-    if (typeof (input) !== 'number') return 'Not a Number'
-    if (input % 3 === 0 && input % 5 === 0) return 'FizzBuzz'
-    if (input % 3 === 0) return 'Fizz'
-    if (input % 5 === 0) return 'Buzz'
+    if (typeof (input) !== 'number')
+        return NaN
 
+    if (input % 3 === 0 && input % 5 === 0)
+        return 'FizzBuzz'
 
+    if (input % 3 === 0)
+        return 'Fizz'
+
+    if (input % 5 === 0)
+        return 'Buzz'
 
     return input
 }
