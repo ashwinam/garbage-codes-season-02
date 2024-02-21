071a538d0e84f038371d81c99c783df2ed6ec298
ashwinPy <ashwinam31@gmail.com>
2024-02-21 12:51:10 +0530

    js-exercise: 01

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/exercise-01.js
@@ -0,0 +1,11 @@
+/* 
+01. Write a function that takes two numbers and return the maximum of two.
+*/
+
+function max_num(first_num, second_num) {
+    if (first_num > second_num) return first_num
+    else
+        return second_num
+}
+
+console.log(max_num(20, 199));
