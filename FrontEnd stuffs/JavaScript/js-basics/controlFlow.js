a5d4a18873b9905ac7ec766829f5a3ad56fd3273
ashwinPy <ashwinam31@gmail.com>
2024-02-21 11:26:44 +0530

    js-basics: control flow(for...of)

--- FrontEnd stuffs/JavaScript/js-basics/controlFlow.js
+++ FrontEnd stuffs/JavaScript/js-basics/controlFlow.js
@@ -79,7 +79,8 @@ for (let key in person) {
     console.log(key, person[key]);
 }
 
-
+// for...of
 const colors = ['red', 'green', 'blue']
 
-for (let index in colors) console.log(index, colors[index]);
+for (let color of colors)
+    console.log(color);
