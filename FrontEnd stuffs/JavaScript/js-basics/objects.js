83f8133ba90e03f518f4baaae2eb905037396eef
ashwinPy <ashwinam31@gmail.com>
2024-02-24 17:52:08 +0530

    js-basics: objects(basics)

---
+++ FrontEnd stuffs/JavaScript/js-basics/objects.js
@@ -0,0 +1,18 @@
+// Objects
+
+// Basic
+
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1,
+    },
+    isVisible: true,
+    draw: function () {
+        console.log('I am drawing');
+    }
+}
+
+
+circle.draw(); // method
