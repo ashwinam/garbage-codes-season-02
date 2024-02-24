0fc46d7b417e4e13385970cc2d82fd09265ef9b3
ashwinPy <ashwinam31@gmail.com>
2024-02-24 18:02:35 +0530

    js-basics: objects(factory function)

--- FrontEnd stuffs/JavaScript/js-basics/objects.js
+++ FrontEnd stuffs/JavaScript/js-basics/objects.js
@@ -16,3 +16,20 @@ const circle = {
 
 
 circle.draw(); // method
+
+// We have two other ways to creating objects 1. Factory function 2. Constructor
+
+// Factory Fnction
+
+function createCircle(radius) {
+    return {
+        radius,
+
+        draw() {
+            console.log('Factory function');
+        }
+    }
+}
+
+const circle1 = createCircle(1);
+console.log(circle1.draw());
