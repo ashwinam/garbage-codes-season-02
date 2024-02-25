f60d02ef468820e7bd4f2597cade50ba15db1c7c
ashwinPy <ashwinam31@gmail.com>
2024-02-25 18:10:39 +0530

    js-exercise: 15(objects)

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/exercise-15.js
@@ -0,0 +1,19 @@
+// Blog post
+
+const blog = {
+    title: "My First Blog",
+    body: "Hello Friends, This is my first blog.",
+    author: "dev_01",
+    comments: [
+        {
+            author: "sekuja01",
+            body: "super blog"
+        },
+        {
+            author: "ronin01",
+            body: "awesome blog"
+        }
+    ],
+    isLive: true
+}
+
