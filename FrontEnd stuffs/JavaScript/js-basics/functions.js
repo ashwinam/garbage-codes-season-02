da1cf3ce58fe7499d2aa028a4dbd4c552c3ae38e
ashwinPy <ashwinam31@gmail.com>
2024-02-27 12:17:39 +0530

    js-basics: Functions(arguments)

--- FrontEnd stuffs/JavaScript/js-basics/functions.js
+++ FrontEnd stuffs/JavaScript/js-basics/functions.js
@@ -17,3 +17,14 @@ let move = run;
 // Hoisting
 
 // So hoisting is the process where you call the function before declaration, so what will happen? --> at a runtime the javascript engine put all the function declaration to the top.
+
+
+// Arguments
+
+// arguments is a special keyword in js that you can pass n number of arguments to the function, and you can pass without argument also;
+
+function sum() {
+    console.log(arguments);
+}
+
+sum(1, 2, 3, 4, 5, 6); 
