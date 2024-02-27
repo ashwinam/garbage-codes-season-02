1159fcbe85f5eb84f9faf926dcc9427bd8407267
ashwinPy <ashwinam31@gmail.com>
2024-02-27 19:22:06 +0530

    js-exercise: 24(Functions)

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/exercise-24.js
@@ -0,0 +1,11 @@
+// Create a sum function
+
+
+function sum(...param) {
+    if (Array.isArray(param[0]))
+        param = param[0]
+
+    return param.reduce((a, b) => a + b)
+}
+
+const output = sum(1, 2, 3, 4, 5);
