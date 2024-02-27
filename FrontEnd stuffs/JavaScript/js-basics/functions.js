dc1b00509d6d0be96e354685047da1604f175e13
ashwinPy <ashwinam31@gmail.com>
2024-02-27 12:01:23 +0530

    js-basics: Functions(declaration and expression)

---
+++ FrontEnd stuffs/JavaScript/js-basics/functions.js
@@ -0,0 +1,14 @@
+// Functions In JavaScript
+
+// Function Declaration
+function walk() {
+    console.log('Walk');
+};
+
+// Anonymous Function Expression
+
+let run = function () {
+    console.log('Run');
+};
+
+let move = run;
