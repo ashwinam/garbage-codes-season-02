2ca91cc219400c9ecd4d4f6f3e01b3bd8d32872f
ashwinPy <ashwinam31@gmail.com>
2024-03-02 12:44:28 +0530

    js-dom: bulb project finished

--- FrontEnd stuffs/JavaScript/js-dom/Bulb/script.js
+++ FrontEnd stuffs/JavaScript/js-dom/Bulb/script.js
@@ -5,7 +5,7 @@
 // 3. Changing CSS
 // 4. Event Listener
 
-
+/*
 let h = document.querySelector('h1'); // selecting element
 const name = "Ashwin";
 h.innerText = `Hello, ${name}`; // Changing HTML
@@ -16,3 +16,20 @@ h.addEventListener("click", () => {
     h.innerText = "This HTML is changed after click."
     console.log('I am clicked.');
 }) // Event Listener
+*/
+
+const bulb = document.querySelector("#bulb");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function () {
+    if (btn.innerText == 'ON') {
+        bulb.style.backgroundColor = "yellow";
+        console.log("On");
+        btn.innerText = "OFF"
+    }
+    else {
+        bulb.style.backgroundColor = "transparent";
+        console.log("OFF");
+        btn.innerText = "ON"
+    }
+})
