50d1a601d81dd10cd6b81cda9c1cb2d0de053030
ashwinPy <ashwinam31@gmail.com>
2024-03-02 19:32:49 +0530

    firstReactApp: rendering list

--- FrontEnd stuffs/React Learning/firstReact-app/src/components/ListGroup.tsx
+++ FrontEnd stuffs/React Learning/firstReact-app/src/components/ListGroup.tsx
@@ -1,13 +1,23 @@
 function ListGroup() {
+  const fruits = [
+    "Banana",
+    "Pineapple",
+    "Pomegranate",
+    "Apple",
+    "Orange",
+    "Grapes",
+  ];
+
   return (
     <>
       <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {/* Here react ask for key attribute to track of whats going on here, or for good implementatio for dom */}
+        {fruits.map((fruit) => (
+          <li className="list-group-item" key={fruit}>
+            {fruit}
+          </li>
+        ))}
       </ul>
     </>
   );
