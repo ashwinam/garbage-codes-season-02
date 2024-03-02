ede08b2cc8fc1a7664f60c19d3359e9dd998f6d2
ashwinPy <ashwinam31@gmail.com>
2024-03-02 12:28:34 +0530

    js-dom: 4 pillars done

--- FrontEnd stuffs/JavaScript/js-dom/Bulb/script.js
+++ FrontEnd stuffs/JavaScript/js-dom/Bulb/script.js
@@ -4,3 +4,15 @@
 // 2. Changing an HTML
 // 3. Changing CSS
 // 4. Event Listener
+
+
+let h = document.querySelector('h1'); // selecting element
+const name = "Ashwin";
+h.innerText = `Hello, ${name}`; // Changing HTML
+
+h.style.color = "lightBlue"; // changing css
+
+h.addEventListener("click", () => {
+    h.innerText = "This HTML is changed after click."
+    console.log('I am clicked.');
+}) // Event Listener
