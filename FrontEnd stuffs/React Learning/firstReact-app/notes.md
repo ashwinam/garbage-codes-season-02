bc07815395a03c9f3e73f0c2d9596d3b4053c86f
ashwinPy <ashwinam31@gmail.com>
2024-03-03 10:45:54 +0530

    firstReact: handling events

--- FrontEnd stuffs/React Learning/firstReact-app/notes.md
+++ FrontEnd stuffs/React Learning/firstReact-app/notes.md
@@ -10,3 +10,6 @@ Suppose, You have a list of items and you want to display them in react, Here we
 
 Conditional Rendering
 In react, Suppose you want to display some values based on conditions or simply a variable inside a JSX(javascript XML) you have to use `{}`.
+
+Handling Events
+For handling events, Inside an JSX elements we have the attribute called `onClick = {pass the function}` for click events and other handling events are also there. and using `event` argument we get a `BaseEvent` object that contains all properties.
--- FrontEnd stuffs/React Learning/firstReact-app/src/components/ListGroup.tsx
+++ FrontEnd stuffs/React Learning/firstReact-app/src/components/ListGroup.tsx
@@ -1,3 +1,6 @@
+import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let fruits = [
     "Banana",
@@ -8,18 +11,25 @@ function ListGroup() {
     "Grapes",
   ];
 
-  fruits = [];
+  // In tyescript we have to provide a type annotation, In react we have MouseEvent using `:`
+  // Event handling
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   // for clean code purpose always use functions for bigger conditions
 
   return (
+    // fragments syntax
     <>
       <h1>List</h1>
       <ul className="list-group">
         {/* Here react ask for key attribute to track of whats going on here, or for good implementatio of dom */}
         {fruits.length === 0 && <p>No Fruits Available</p>}
-        {fruits.map((fruit) => (
-          <li className="list-group-item" key={fruit}>
+        {fruits.map((fruit, index) => (
+          <li
+            className="list-group-item"
+            key={fruit}
+            onClick={handleClick} // here we pass reference
+          >
             {fruit}
           </li>
         ))}
