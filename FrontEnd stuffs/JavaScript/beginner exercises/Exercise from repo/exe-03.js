8d9c208156432c694b40cca8c873e153c59b651a
ashwinPy <ashwinam31@gmail.com>
2024-03-04 12:00:09 +0530

    js-exercise: repo (03)

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-03.js
@@ -0,0 +1,33 @@
+// 3. Clock
+
+/*
+The clock shows h hours (0 <= h <= 23), m minutes (0 <= m <= 59) and s seconds (0 <= s <= 59) after midnight. Your task is to write a function which returns the time since midnight in milliseconds. There are 1,000 milliseconds in a second.
+*/
+
+const past = (h, m, s) => {
+    const second = 1000
+    let output = 0;
+    let m_s = 60;
+    let h_m = 60;
+    if (s)
+        output = output + (s * second);
+
+    if (m) {
+        // convert to seconds
+
+        output = output + ((m * m_s) * second);
+    }
+
+    if (h) {
+        output = output + ((h * h_m * m_s) * second);
+    }
+
+
+    return output
+};
+
+console.log(past(0, 0, 0)); // 0
+console.log(past(0, 1, 1)); // 61000
+console.log(past(1, 0, 0)); // 3600000
+console.log(past(1, 0, 1)); // 3601000
+console.log(past(1, 1, 1)); // 3661000
