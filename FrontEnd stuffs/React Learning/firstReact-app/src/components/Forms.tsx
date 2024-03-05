5ca6630690bfeaf66b2a52889515dc61e429f7e5
ashwinPy <ashwinam31@gmail.com>
2024-03-05 15:21:29 +0530

    firstReact: react hook forms

--- FrontEnd stuffs/React Learning/firstReact-app/src/components/Forms.tsx
+++ FrontEnd stuffs/React Learning/firstReact-app/src/components/Forms.tsx
@@ -1,35 +1,48 @@
 import { FormEvent, useRef } from "react";
+import { useForm } from "react-hook-form";
 
 const Forms = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
+  const { register, handleSubmit } = useForm();
 
-  const person = {
-    name: "",
-    age: 0,
-  };
+  //   const nameRef = useRef<HTMLInputElement>(null);
+  //   const ageRef = useRef<HTMLInputElement>(null);
 
-  const handleForms = (event: FormEvent) => {
-    event.preventDefault();
-    if (nameRef.current !== null) person.name = nameRef.current.value;
-    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
-  };
+  //   const person = {
+  //     name: "",
+  //     age: 0,
+  //   };
+
+  //   const handleForms = (event: FormEvent) => {
+  //     event.preventDefault();
+  //     if (nameRef.current !== null) person.name = nameRef.current.value;
+  //     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+  //   };
 
   return (
     <>
-      <form action="" onSubmit={handleForms}>
+      <form action="" onSubmit={handleSubmit((data) => console.log(data))}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
             Name
           </label>
-          <input ref={nameRef} id="name" type="text" className="form-control" />
+          <input
+            {...register("name")}
+            id="name"
+            type="text"
+            className="form-control"
+          />
         </div>
 
         <div className="mb-3">
           <label htmlFor="age" className="form-label">
             Age
           </label>
-          <input ref={ageRef} id="age" type="number" className="form-control" />
+          <input
+            {...register("age")}
+            id="age"
+            type="number"
+            className="form-control"
+          />
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
