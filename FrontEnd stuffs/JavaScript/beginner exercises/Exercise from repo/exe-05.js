bf41db9cc008a1cbf80f3332d29c52f94fb38f97
ashwinPy <ashwinam31@gmail.com>
2024-03-06 18:35:42 +0530

    js-exercise: repo(05)

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-05.js
@@ -0,0 +1,18 @@
+// 5. Century From Year
+
+/* 
+The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc. Given a year, return the century it is in.
+*/
+
+const century = year => {
+    if (year <= 100) return 1;
+    if (year % 100)
+        return (Math.floor(year / 100)) + 1;
+    return year / 100;
+};
+
+console.log(century(1705)); // 18
+console.log(century(1900)); // 19
+console.log(century(1601)); // 17
+console.log(century(2000)); // 20
+console.log(century(89)); // 1
