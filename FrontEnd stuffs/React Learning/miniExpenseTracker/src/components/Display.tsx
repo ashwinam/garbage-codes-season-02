a131c07d5ba47a15c86c4ccc609bfaaa7e86d74c
ashwinPy <ashwinam31@gmail.com>
2024-03-06 20:06:50 +0530

    miniExpenseTracker: UI finished

--- FrontEnd stuffs/React Learning/miniExpenseTracker/src/components/Display.tsx
+++ FrontEnd stuffs/React Learning/miniExpenseTracker/src/components/Display.tsx
@@ -11,6 +11,28 @@ function Display() {
         <option value="Groceries">Groceries</option>
         <option value="Entertainment">Entertainment</option>
       </select>
+      <div className="mb-3">
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Amount</th>
+              <th>Caegory</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>
+                <button className="btn btn-outline-danger">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
