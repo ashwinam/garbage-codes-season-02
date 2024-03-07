eadc8b4fe395ecbe4790f66da51fc44d44719958
ashwinPy <ashwinam31@gmail.com>
2024-03-07 11:44:35 +0530

    miniExpenseTracker: form validation and react hook form

--- FrontEnd stuffs/React Learning/miniExpenseTracker/src/components/Forms.tsx
+++ FrontEnd stuffs/React Learning/miniExpenseTracker/src/components/Forms.tsx
@@ -1,23 +1,34 @@
+import { useForm } from "react-hook-form";
+
 const Forms = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
     <>
-      <form
-        action=""
-        onSubmit={(event) => {
-          event.preventDefault();
-          console.log("Clicked");
-        }}
-      >
+      <form action="" onSubmit={handleSubmit((data) => console.log(data))}>
         <div className="mb-3">
           <label htmlFor="description" className="form-label fs-4">
             Description
           </label>
           <input
+            {...register("description", { required: true, minLength: 3 })}
             id="description"
             type="text"
             placeholder="Description"
             className="form-control form-control-lg"
           />
+          {errors.description?.type === "required" && (
+            <p className="text-danger">Description is required.</p>
+          )}
+          {errors.description?.type === "minLength" && (
+            <p className="text-danger">
+              Description is atleast 3 characters long.
+            </p>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="amount" className="form-label fs-4">
@@ -25,10 +36,14 @@ const Forms = () => {
           </label>
           <input
             type="number"
+            {...register("amount", { required: true })}
             placeholder="Enter Amount"
             className="form-control form-control-lg"
             id="amount"
           />
+          {errors.amount?.type === "required" && (
+            <p className="text-danger">Amount is required.</p>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="categories" className="form-label fs-4">
@@ -36,17 +51,21 @@ const Forms = () => {
           </label>
           <select
             id="categories"
+            {...register("categories", { required: true })}
             className="form-select form-select-lg"
             aria-label="Default select example"
-            defaultValue={"default"}
+            defaultValue={""}
           >
-            <option value="default" disabled hidden>
+            <option value="" disabled hidden>
               Select Category
             </option>
             <option value="Utilities">Utilities</option>
             <option value="Groceries">Groceries</option>
             <option value="Entertainment">Entertainment</option>
           </select>
+          {errors.categories?.type === "required" && (
+            <p className="text-danger">Catoegories is required.</p>
+          )}
         </div>
         <button type="submit" className="btn btn-lg btn-primary">
           Submit
