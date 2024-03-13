8b9d550d28a4117192452ac91286df4ba6b5dbcc
ashwinPy <ashwinam31@gmail.com>
2024-03-13 11:09:45 +0530

    js-exercise: repo(09) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-09.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-09.js
@@ -24,4 +24,10 @@ const disemvowel = str => {
     return newStr;
 };
 
+// Author solution
+// const disemvowel = str => {
+//     // Let's use regular expressions (regex)
+//     return str.replace(/[aeiou]/gi, '');
+//   };
+
 console.log(disemvowel('This website is for losers LOL!')); // 'Ths wbst s fr lsrs LL!'
