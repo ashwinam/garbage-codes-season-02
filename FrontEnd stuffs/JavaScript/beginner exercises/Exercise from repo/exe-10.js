07c6995ce54d99421f100e6b94c44493d9619d9f
ashwinPy <ashwinam31@gmail.com>
2024-03-14 12:10:53 +0530

    js-exercise: repo(10) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-10.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-10.js
@@ -22,6 +22,12 @@ const findOdd = arr => {
     return oddNum;
 };
 
+// Author solution
+/*
+const findOdd = arr => {
+  return arr.reduce((a, b) => a ^ b);
+}; */
+
 console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])); // 5
 console.log(findOdd([20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5])); // 5
 console.log(findOdd([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5])); // -1
