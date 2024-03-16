774482d2cc9cc029e65521c1d4513cc8e19f04c8
ashwinPy <ashwinam31@gmail.com>
2024-03-16 12:50:56 +0530

    js-exercise: repo(12) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-12.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-12.js
@@ -14,6 +14,27 @@ const likes = names => {
     return names.join() + ' likes this.'
 };
 
+// Authors solution
+/*
+const likes = names => {
+  const len = names.length;
+  let output;
+  if (len === 0) {
+    output = 'no one likes this';
+  } else if (len === 1) {
+    output = `${names[0]} likes this`;
+  } else if (len === 2) {
+    output = `${names[0]} and ${names[1]} like this`;
+  } else if (len === 3) {
+    output = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    output = `${names[0]}, ${names[1]} and ${len - 2} others like this`;
+  }
+
+  return output;
+};
+*/
+
 console.log(likes([])); // 'no one likes this'
 console.log(likes(['Peter'])); // 'Peter likes this'
 console.log(likes(['Jacob', 'Alex'])); // 'Jacob and Alex like this'
