de86b0cc8a39957e39fb4a824a3cbe136332726e
ashwinPy <ashwinam31@gmail.com>
2024-03-17 11:48:50 +0530

    js-exercise: repo(13) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-13.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-13.js
@@ -8,6 +8,21 @@ const createPhoneNumber = numbers => {
     return `(${numbers.slice(0, 3).join('')}) ${numbers.slice(3, 6).join('')}-${numbers.slice(6).join('')}`
 };
 
+// Author solution
+/*
+const createPhoneNumber = numbers => {
+  // Using substrings
+  const str = numbers.join('');
+  return `(${str.substring(0, 3)}) ${str.substring(3, 6)}-${str.substring(6)}`;
+
+  // Alternative solution using RegEx
+  // return numbers.join('').replace(/(\d{3})(\d{3})(\d+)/, '($1) $2-$3');
+
+  // Alternative solution using reduce()
+  // return numbers.reduce((acc, cur) => acc.replace('x', cur), '(xxx) xxx-xxxx');
+};
+*/
+
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // '(123) 456-7890'
 console.log(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])); // '(111) 111-1111'
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // '(123) 456-7890'
