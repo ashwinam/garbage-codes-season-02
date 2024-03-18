b98713896676eb3c921b71ce3bc4528f01fc3c94
ashwinPy <ashwinam31@gmail.com>
2024-03-18 11:27:09 +0530

    js-exercise: repo(14) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-14.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-14.js
@@ -9,6 +9,19 @@ const squareDigits = num => {
     return arrOfNumbers.map(num => parseInt(num) * parseInt(num)).join('')
 };
 
+// Author solution
+/*
+const squareDigits = num => {
+  return Number(
+    num
+      .toString()
+      .split('')
+      .map(ele => ele * ele)
+      .join('')
+  );
+};
+*/
+
 console.log(squareDigits(2112)); // 4114
 console.log(squareDigits(3212)); // 9414
 console.log(squareDigits(9159)); // 8112581
