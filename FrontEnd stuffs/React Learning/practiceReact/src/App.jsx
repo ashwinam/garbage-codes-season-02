0cbe3936ab753359210a08203d2021233af4692c
ashwinPy <ashwinam31@gmail.com>
2024-03-18 19:29:48 +0530

    foreground component

--- FrontEnd stuffs/React Learning/practiceReact/src/App.jsx
+++ FrontEnd stuffs/React Learning/practiceReact/src/App.jsx
@@ -1,11 +1,15 @@
 import Background from "./components/Background"
+import Foreground from "./components/Foreground"
 
 function App() {
 
 
-  return <div className="relative w-full h-screen bg-zinc-900">
-    <Background />
-  </div>
+  return (
+    <div className="relative w-full h-screen bg-zinc-900">
+      <Background />
+      <Foreground />
+    </div>
+  )
 }
 
 export default App
---
+++ FrontEnd stuffs/React Learning/practiceReact/src/components/Foreground.jsx
@@ -0,0 +1,8 @@
+
+function Foreground() {
+    return (
+        <div className="fixed top-0 left-0 w-full h-screen z-[3]"></div>
+    )
+}
+
+export default Foreground
