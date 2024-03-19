caa38fd9b4b0251fffa92e59c6d76b39560bc203
ashwinPy <ashwinam31@gmail.com>
2024-03-19 20:01:35 +0530

    phase1: first phase complete in docs

--- FrontEnd stuffs/React Learning/practiceReact/src/components/Cards.jsx
+++ FrontEnd stuffs/React Learning/practiceReact/src/components/Cards.jsx
@@ -1,23 +1,30 @@
+
 import { FaRegFileLines } from "react-icons/fa6";
 import { RxCross2 } from "react-icons/rx";
 import { FiDownload } from "react-icons/fi";
+import { motion } from "framer-motion"
 
 
-function Cards({ data }) {
+function Cards({ data, reference }) {
     return (
-        <div className="cards relative w-60 h-72 bg-zinc-800/10 mt-5 rounded-[35px] text-white px-8 py-10 overflow-hidden">
+        <motion.div drag dragMomentum={true} dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }} whileDrag={{ scale: 1.2 }} dragConstraints={reference} className="cards relative w-60 h-72 bg-zinc-800/10 mt-5 rounded-[35px] text-white px-8 py-10 overflow-hidden">
             <FaRegFileLines />
             <p className="text-lg mt-3 font-semibold">{data.desc}</p>
-            <div className={`footer absolute bottom-0 left-0 w-full ${data.isDownloadFinished ? 'mb-10' : null} flex items-center justify-between p-5`}>
+            <div className={`footer absolute bottom-0 left-0 w-full mb-10 flex items-center justify-between p-5 bg-zinc-800/10`}>
                 <div className="data-size font-roboto font-semibold ">{data.fileSize}</div>
                 <span className="bg-zinc-200 p-1 rounded-full cursor-pointer">
                     {data.isDownloadFinished ? <RxCross2 color="black" size="1em" /> : <FiDownload color="black" size="1em" />}
                 </span>
             </div>
-            <div className="tag absolute bottom-0 left-0 w-full h-12 bg-green-600 flex items-center justify-center font-roboto font-semibold">
-                {data.isDownloadFinished ? 'Open Now' : <progress value={data.tag.downloadPercentage} color="white" />}
+            <div className={`tag absolute bottom-0 left-0 w-full h-12 ${data.isDownloadFinished ? 'bg-green-600 cursor-pointer' : 'bg-blue-600'} flex items-center justify-center font-roboto font-semibold`}>
+                {data.tag.isDownloadStart ? <progress value={data.tag.downloadPercentage} className="progressBar" style={{
+                    width: '180px',
+                    height: '10px',
+                    backgroundColor: 'white',
+                }}
+                /> : 'Click to open'}
             </div>
-        </div>
+        </motion.div>
     )
 }
 
--- FrontEnd stuffs/React Learning/practiceReact/src/components/Foreground.jsx
+++ FrontEnd stuffs/React Learning/practiceReact/src/components/Foreground.jsx
@@ -1,6 +1,10 @@
 import Cards from "./Cards"
+import { useRef } from "react";
+
+
 
 function Foreground() {
+    const constraintsRef = useRef(null)
 
     const data = [
         {
@@ -9,25 +13,25 @@ function Foreground() {
             tag: { isDownloadStart: false, downloadPercentage: 0.5, tagTitle: 'Open' }
         },
         {
-            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.", fileSize: '0.4MB',
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.", fileSize: '3.4MB',
             isDownloadFinished: true,
             tag: { isDownloadStart: false, downloadPercentage: 0.5, tagTitle: 'Open' }
         },
         {
-            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.", fileSize: '0.4MB',
-            isDownloadFinished: true,
-            tag: { isDownloadStart: false, downloadPercentage: 0.5, tagTitle: 'Open' }
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.", fileSize: '2.9MB',
+            isDownloadFinished: false,
+            tag: { isDownloadStart: true, downloadPercentage: 0.7, tagTitle: 'Open' }
         },
         {
-            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.", fileSize: '0.4MB',
+            desc: "Lorem ipsum dolor sit, amet consectetur adipisicing.", fileSize: '1.4MB',
             isDownloadFinished: true,
             tag: { isDownloadStart: false, downloadPercentage: 0.5, tagTitle: 'Open' }
         },
     ]
 
     return (
-        <div className="fixed top-0 left-0 w-full h-screen z-[3] flex gap-10">
-            {data.map((item, index) => <Cards key={index} data={item} />)}
+        <div ref={constraintsRef} className="fixed top-0 left-0 w-full h-screen z-[3] flex gap-10">
+            {data.map((item, index) => <Cards key={index} reference={constraintsRef} data={item} />)}
         </div>
     )
 }
