65baac9f6c04a26d0651af529f90aab1e1a02478
ashwinPy <ashwinam31@gmail.com>
2024-03-20 12:15:24 +0530

    js-exercise: repo(16) Author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-16.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-16.js
@@ -14,6 +14,14 @@ const highAndLow = numbers => {
     return outpt
 };
 
+// Author solution
+/*
+const highAndLow = numbers => {
+  const arr = numbers.split(' ');
+  return `${Math.max(...arr)} ${Math.min(...arr)}`;
+};
+*/
+
 console.log(highAndLow('1 2 3 4 5')); // '5 1'
 console.log(highAndLow('1 2 -3 4 5')); // '5 -3'
 console.log(highAndLow('1 9 3 4 -5')); // '9 -5'
