fe3faa52b18a2634481740a807ab62492fa47812
ashwinPy <ashwinam31@gmail.com>
2024-03-23 11:26:15 +0530

    js-exercise: repo(19) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-19.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-19.js
@@ -15,6 +15,17 @@ const spinWords = str => {
     return mappedList.join(' ')
 };
 
+// Author solution
+
+/*
+const spinWords = str => {
+  return str
+    .split(' ')
+    .map(word => (word.length < 5 ? word : word.split('').reverse().join('')))
+    .join(' ');
+};
+*/
+
 console.log(spinWords('This is a test')); // 'This is a test'
 console.log(spinWords('Hey fellow warriors')); // 'Hey wollef sroirraw'
 console.log(spinWords('This is another test')); // 'This is rehtona test'
