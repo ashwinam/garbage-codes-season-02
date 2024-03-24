a9fa90737b66c440d0ed29a70df491de28d5126e
ashwinPy <ashwinam31@gmail.com>
2024-03-24 11:47:04 +0530

    js-exercise: repo(20) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-20.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-20.js
@@ -8,6 +8,14 @@ const findShort = str => {
     return str.split(' ').map(word => word.length).sort()[0]
 };
 
+// Author solution
+
+/*
+const findShort = str => {
+  return Math.min(...str.split(' ').map(word => word.length));
+};
+*/
+
 console.log(findShort('Test where final word shortest see')); // 3
 console.log(findShort('Lets all go on holiday somewhere very cold')); // 2
 console.log(findShort('i want to travel the world writing code one day')); // 1
