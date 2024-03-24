b73530f06587af7cb9bd2178b3d7b3478fdd4cea
ashwinPy <ashwinam31@gmail.com>
2024-03-24 11:45:19 +0530

    js-exercise: repo(20)

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-20.js
@@ -0,0 +1,13 @@
+// 20. Shortest Word
+
+/*
+Given a non-empty string of words, return the length of the shortest word(s).
+*/
+
+const findShort = str => {
+    return str.split(' ').map(word => word.length).sort()[0]
+};
+
+console.log(findShort('Test where final word shortest see')); // 3
+console.log(findShort('Lets all go on holiday somewhere very cold')); // 2
+console.log(findShort('i want to travel the world writing code one day')); // 1
