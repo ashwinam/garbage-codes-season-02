08a2912943e0bb996e3c0e8e4dd41d0a037ea4b1
ashwinPy <ashwinam31@gmail.com>
2024-03-26 12:50:35 +0530

    js-exercise: repo(21) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-21.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-21.js
@@ -25,6 +25,13 @@ const countBits = n => {
 
 };
 
+// Author solution
+/*
+const countBits = n => {
+  return n.toString(2).split('0').join('').length;
+};
+*/
+
 console.log(countBits(0)); // 0
 console.log(countBits(4)); // 1
 console.log(countBits(7)); // 3
