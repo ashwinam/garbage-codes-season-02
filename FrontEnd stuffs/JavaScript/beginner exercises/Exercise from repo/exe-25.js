1f6cec4d4324caa13427f6482d39862e67f62e3c
ashwinPy <ashwinam31@gmail.com>
2024-03-31 11:41:48 +0530

    js-exercise: repo(25)

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-25.js
@@ -0,0 +1,14 @@
+// 25. Array.diff
+
+/*
+Write a function that subtracts one list from another and returns the result. It should remove all values from array a, which are present in array b.
+*/
+
+const arrayDiff = (a, b) => {
+    return a.filter((ele, index) => ele != b[index])
+};
+
+console.log(arrayDiff([1, 8, 2], [])); // [1, 8, 2]
+console.log(arrayDiff([1, 2, 3], [1, 2])); // [3]
+console.log(arrayDiff([3, 4], [3])); // [4]
+console.log(arrayDiff([], [4, 5])); // []
