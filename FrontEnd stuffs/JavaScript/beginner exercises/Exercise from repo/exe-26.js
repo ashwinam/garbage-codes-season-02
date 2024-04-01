8e0d2fc879efaca5c66c0cc6adc18f67f08bca28
ashwinPy <ashwinam31@gmail.com>
2024-04-01 11:37:41 +0530

    js-exercise: repo(26) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-26.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-26.js
@@ -9,5 +9,15 @@ String.prototype.capitalize = function () {
     return value.split(" ").map(ele => ele.replace(ele, ele[0].toUpperCase() + ele.slice(1))).join(" ");
 };
 
+// Author Solution
+
+/*
+String.prototype.capitalize = function () {
+  return this.split(' ')
+    .map(ele => ele[0].toUpperCase() + ele.slice(1))
+    .join(' ');
+};
+*/
+
 var str = "How can mirrors be real if our eyes aren't real";
 console.log(str.capitalize()); // 'How Can Mirrors Be Real If Our Eyes Aren't Real'
