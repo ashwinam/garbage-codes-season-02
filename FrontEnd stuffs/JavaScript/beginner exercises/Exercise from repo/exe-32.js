baedd16d5c113f777a14486c4892763c09cc1c9f
ashwinPy <ashwinam31@gmail.com>
2024-04-13 11:05:55 +0530

    js-exercise: repo(32)

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-32.js
@@ -0,0 +1,18 @@
+// 32. Reversed Strings
+
+/*
+Write a function that reverses the string that is passed to it. For this challenge, you may NOT use the JavaScript built-in reverse() method.
+*/
+
+const reverseString = str => {
+    let reversed_string = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed_string += str[i];
+    }
+    return reversed_string;
+};
+
+console.log(reverseString('hello')); // 'olleh'
+console.log(reverseString('world')); // 'dlrow'
+console.log(reverseString('')); // ''
+console.log(reverseString('h')); // 'h'
