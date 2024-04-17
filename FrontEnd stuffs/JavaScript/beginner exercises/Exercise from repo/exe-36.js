4bcbe3e7af50ec69127d59ca100bf338d89adce6
ashwinPy <ashwinam31@gmail.com>
2024-04-17 11:14:06 +0530

    js-exercise: repo(36) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-36.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-36.js
@@ -13,5 +13,29 @@ const twoSum = (nums, target) => {
 
 };
 
+// Author solution
+/*
+const twoSum = (nums, target) => {
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    const j = nums.lastIndexOf(target - nums[i]);
+    if (j > i) {
+      return [i, j];
+    }
+  }
+
+  // Alternative solution using Map
+  // const map = new Map();
+
+  // for (let i = 0; i < nums.length; i++) {
+  //   const otherIndex = map.get(target - nums[i]);
+  //   if (typeof otherIndex !== 'undefined') {
+  //     return [otherIndex, i];
+  //   }
+  //   map.set(nums[i], i);
+  // }
+};
+*/
+
 console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
 console.log(twoSum([3, 2, 4], 6)); // [1, 2]
