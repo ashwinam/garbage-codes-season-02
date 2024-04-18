c1761474f2822c0ec6615d2bdb5f1884f8b052a7
ashwinPy <ashwinam31@gmail.com>
2024-04-18 11:15:58 +0530

    js-exercise: repo(37)

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-37.js
@@ -0,0 +1,19 @@
+// 37. Unique In Order
+
+/*
+Implement a function that takes an iterable argument (a string or an array) as input and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+*/
+
+const uniqueInOrder = iterable => {
+    let output = [];
+    for (let i in iterable) {
+        if (iterable[i] !== iterable[parseInt(i) + 1]) {
+            output.push(iterable[i])
+        }
+    }
+    return output
+};
+
+console.log(uniqueInOrder([1, 2, 2, 3, 3])); // [1, 2, 3]
+console.log(uniqueInOrder('ABBCcAD')); // ['A', 'B', 'C', 'c', 'A', 'D']
+console.log(uniqueInOrder('AAAABBBCCDAABBB')); // ['A', 'B', 'C', 'D', 'A', 'B']
