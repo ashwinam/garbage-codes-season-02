ff922fc665cebe763d849d4988e6af29c6ef324d
Ashwin <ashwinam31@gmail.com>
2024-04-28 16:53:46 +0530

    js-exercise: repo(43) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-43.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-43.js
@@ -19,6 +19,15 @@ Explanation:
 const kidsWithCandies = (candies, extraCandies) => {
     return candies.map((ele => ele + extraCandies > Math.max(...candies)))
   };
+
+// Author solution
+
+/*
+const kidsWithCandies = (candies, extraCandies) => {
+  const MAX = Math.max(...candies);
+  return candies.map(candy => candy + extraCandies >= MAX);
+};
+*/
   
 console.log(kidsWithCandies([12, 1, 12], 10)); // [true, false, true]
 console.log(kidsWithCandies([4, 2, 1, 1, 2], 1)); // [true, false, false, false, false]
