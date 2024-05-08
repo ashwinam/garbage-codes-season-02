609d4ada6a0eef2436aea136085190eb35432963
Ashwin <ashwinam31@gmail.com>
2024-05-08 10:27:00 +0530

    js-exercise: repo(52)

---
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-52.js
@@ -0,0 +1,22 @@
+// 52. First Non-repeating Character
+
+/*
+Write a function that takes an input string and returns the first character that is not repeated anywhere in the string. Upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter.
+*/
+
+const firstNonRepeatingLetter = str => {
+    for (let char of str){
+        smallChar = char.toLowerCase();
+        let lowerCaseStr = str.toLowerCase()
+        if (lowerCaseStr.indexOf(smallChar) === lowerCaseStr.lastIndexOf(smallChar)){
+            return char;
+        }
+    }
+  };
+  
+  console.log(firstNonRepeatingLetter('a')); // 'a'
+  console.log(firstNonRepeatingLetter('stress')); // 't'
+  console.log(firstNonRepeatingLetter('sTreSS')); // 'T'
+  console.log(firstNonRepeatingLetter('abba')); // ''
+  console.log(firstNonRepeatingLetter("Go hang a salami, I'm a lasagna hog!")); // ','
+
