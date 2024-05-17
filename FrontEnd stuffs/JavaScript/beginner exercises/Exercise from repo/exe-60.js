56adbb56b04df9b6f2c64ba3199a8f4845cd5eab
Ashwin <ashwinam31@gmail.com>
2024-05-17 10:04:13 +0530

    js-exercise: repo(60) author solution

--- FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-60.js
+++ FrontEnd stuffs/JavaScript/beginner exercises/Exercise from repo/exe-60.js
@@ -9,6 +9,26 @@ const moveZeroes = nums => {
     let nonZeros = nums.filter(ele => ele);
     return nonZeros.concat(zeros);
   };
+
+//   Author solution
+/*
+const moveZeroes = nums => {
+  const length = nums.length;
+  let index = 0;
+
+  for (let i = 0; i < length; i++) {
+    if (nums[i] !== 0) {
+      nums[index] = nums[i];
+      if (index !== i) {
+        nums[i] = 0;
+      }
+      index++;
+    }
+  }
+
+  return nums;
+};
+*/
   
   console.log(moveZeroes([0, 1, 0, 3, 12])); // [1, 3, 12, 0, 0]
   console.log(moveZeroes([0, 0, 1])); // [1, 0, 0]
